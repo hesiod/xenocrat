9004d121a1c08e9fcbcee3c399c079070145cd64
Tobias Markus <tobias@miglix.eu>
2015-02-04 09:25:24 +0100

    Change camera perspective; refactoring
    Signed-off-by: Tobias Markus <tobias@miglix.eu>

--- src-exec/GL.hs
+++ src-exec/GL.hs
@@ -35,38 +35,38 @@ displayState :: IORef (State (Pair GLfloat)) -> IORef (Screen GLfloat) -> IO ()
 displayState state screen = do
   s <- get state
   scr <- get screen
-
-  blend $= Enabled
-  blendFunc $= (SrcAlpha, OneMinusSrcAlpha)
-
-  clearColor $= Color4 1 1 1 0
+--blend $= Enabled
+--blendFunc $= (SrcAlpha, OneMinusSrcAlpha)
+  clearColor $= white
   clear [ColorBuffer]
-  loadIdentity
 
   matrixMode $= Projection
   loadIdentity
-  let near = 0
+  let near = 1
       far = 5
       right = 1
       top = 1
   frustum (-right) right (-top) top near far
   matrixMode $= Modelview 0
-
+  loadIdentity
+  lookAt (Vertex3 1 1 1) (Vertex3 0 0 0) (Vector3 0 1 0)
+  rotate (90 :: GLfloat) $ Vector3 1 0 0
   displayCross
-  picturizeState scr (3e6,1e1) s
+  picturizeState scr (0.3*3e6,1e1) s
   swapBuffers
 
 displayCross :: IO ()
 displayCross = do
-  loadIdentity
-  color red
-  lineWidth $= 0.5
-  renderPrimitive Lines $ do
-    vertex (Vertex3 (-1) 0 0 :: Vertex3 GLfloat)
-    vertex (Vertex3 1 0 0    :: Vertex3 GLfloat)
-  renderPrimitive Lines $ do
-    vertex (Vertex3 0 (-1) 0 :: Vertex3 GLfloat)
-    vertex (Vertex3 0 1 0    :: Vertex3 GLfloat)
-  renderPrimitive Lines $ do
-    vertex (Vertex3 0 0 (-1) :: Vertex3 GLfloat)
-    vertex (Vertex3 0 0 1    :: Vertex3 GLfloat)
+  preservingMatrix $ do
+    color red
+    lineWidth $= 0.8
+    let l = 1000
+    renderPrimitive Lines $ do
+      vertex (Vertex3 (-l) 0 0 :: Vertex3 GLfloat)
+      vertex (Vertex3 l 0 0    :: Vertex3 GLfloat)
+    renderPrimitive Lines $ do
+      vertex (Vertex3 0 (-l) 0 :: Vertex3 GLfloat)
+      vertex (Vertex3 0 l 0    :: Vertex3 GLfloat)
+    renderPrimitive Lines $ do
+      vertex (Vertex3 0 0 (-l) :: Vertex3 GLfloat)
+      vertex (Vertex3 0 0 l    :: Vertex3 GLfloat)
--- src-exec/RenderGL.hs
+++ src-exec/RenderGL.hs
@@ -20,38 +20,39 @@ scaleCoordinates scr zoom v = (a/) *** (b/) $ scr
     where
       (a:b:_) = map snd . decompose . (^/zoom) $ v
 
-black, blue, red, green :: Color4 GLfloat
+black, white, blue, red, green :: Color4 GLfloat
 black = Color4 0 0 0 1
+white = Color4 1 1 1 1
 blue = Color4 0 0 1 1
 red = Color4 1 0 0 1
 green = Color4 0 1 0 1
 
 picturizeV :: (HasBasis v, AdditiveGroup v, Show s, Fractional s, MatrixComponent s, VertexComponent s, s ~ Scalar v) => Screen s -> Zoom s -> Body v -> IO ()
-picturizeV scr zoom b = do
-  loadIdentity
-  lineWidth $= 2
-  translate $ Vector3 xR yR 0
-  currentColor $= black
-  renderObject Solid $ Sphere' 0.01 16 16
---  renderQuadric style $ Sphere 0.1 30 30
-  currentColor $= blue
-  renderPrimitive Lines $ do
-    vertex nullV
-    vertex $ Vertex3 xV yV 0
-  translate $ Vector3 xV yV 0
-  rotate deg $ Vector3 0 0 (1::GLfloat)
-  renderPrimitive Lines $ do
-    vertex nullV
-    vertex $ Vertex3 xA yA 0
-  rotate deg' $ Vector3 0 0 (1::GLfloat)
-  renderPrimitive Lines $ do
-    vertex nullV
-    vertex $ Vertex3 xA yA 0
+picturizeV scr zoom b =
+    preservingMatrix $ do
+      lineWidth $= 2
+      translate $ Vector3 xR yR 0
+      currentColor $= black
+      renderObject Solid $ Sphere' 0.01 16 16
+      currentColor $= blue
+      renderPrimitive Lines $ do
+                         vertex nullV
+                         vertex $ Vertex3 xV yV 0
+      translate $ Vector3 xV yV 0
+      rotate deg $ Vector3 0 0 (1::GLfloat)
+      renderPrimitive Lines $ do
+                         vertex nullV
+                         vertex $ Vertex3 xA yA 0
+      rotate deg' $ Vector3 0 0 (1::GLfloat)
+      renderPrimitive Lines $ do
+                         vertex nullV
+                         vertex $ Vertex3 xA yA 0
   where
-      fac = 1/0.07
+      fac = 1/0.3
       deg = 135 + 10
       deg' = 360 - 2*deg
---      style = QuadricStyle (Just Smooth) NoTextureCoordinates Outside LineStyle
+--    style = QuadricStyle (Just Smooth) NoTextureCoordinates Outside LineStyle
+--    renderQuadric style $ Sphere 0.1 30 30
       nullV = Vertex3 0 0 0 :: Vertex3 GLfloat
       (zoomR, zoomV) = zoom
       (xR, yR) = scaleCoordinates scr zoomR (pos b)
